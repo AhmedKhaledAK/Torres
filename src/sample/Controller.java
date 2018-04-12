9f1e48b73475c14997e4dae5d2e374a964b7f6c7
alyaazab <alya.113@live.com>
2018-04-12 19:27:13 +0200

    SceneGraph: Brush+Eraser+Line+Circle+Rectangle
    Uses colorPicker and sliderSize for each drawn object.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,26 +2,20 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.*;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import sample.lang.Lang;
 
 public class Controller {
 
     @FXML
     private ColorPicker colorPicker;
-
-    /*@FXML
-    private TextField txtBrushSize;*/
-
-    @FXML
-    private Canvas canvas;
 
     @FXML
     private CheckBox checkBoxEraser;
@@ -32,91 +26,158 @@ public class Controller {
     @FXML
     Pane pane;
 
-
-    private boolean toolSelected = false;
+    private boolean brushSelected = false;
     private boolean lineSelected = false;
-
-    private GraphicsContext gcBrushTool;
+    private boolean circleSelected = false;
+    private boolean rectangleSelected = false;
 
     Line line=null;
+    Circle circle = null;
+    Circle brushStroke = null;
+    Rectangle rectangle = null;
 
 
-    /**Button Clicks*/
+    private double x=0,y=0;
+
     @FXML
     public void onBtnBrushClick(ActionEvent actionEvent) {
-        toolSelected ^= true;
+        brushSelected ^= true;
     }
 
     @FXML
     public void onBtnLineClick(ActionEvent actionEvent) {
         lineSelected ^= true;
     }
-    /**End of Button Clicks*/
 
     @FXML
-    public void initialize(){
-        gcBrushTool = canvas.getGraphicsContext2D();
-        canvas.setOnMousePressed(e -> {
-            if(lineSelected){
-                x = e.getX();
-                y = e.getY();
-                x1 = e.getX();
-                y1 = e.getY();
-                reDraw(gcBrushTool);
-            }
-        });
-        canvas.setOnMouseReleased(e -> {
-            if (lineSelected) {
-                x1 = e.getX();
-                y1 = e.getY();
-                reDraw(gcBrushTool);
-            }
-        });
-        canvas.setOnMouseDragged(e -> {
-            if (lineSelected) {
-                pane.getChildren().removeAll(line);
-                x1 = e.getX();
-                y1 = e.getY();
-                flag = 1;
-                reDraw(gcBrushTool);
-            }
-            double size = 0;
-            try {
-                size = Double.parseDouble(String.valueOf(sliderSize.getValue()).trim());
-            } catch (NumberFormatException e1) {
-                Lang.showDiag(Alert.AlertType.ERROR, "Error", "Size is not a number", "Enter a valid size!");
-            }
-            double x = e.getX() - size/2;
-            double y = e.getY() - size/2;
-
-            if(toolSelected && !checkBoxEraser.isSelected())
-            {
-                gcBrushTool.setFill(colorPicker.getValue());
-                gcBrushTool.fillRoundRect(x, y, size, size, size, size);
-            }
-            else if(checkBoxEraser.isSelected())
-            {
-                gcBrushTool.setFill(Color.WHITE);
-                gcBrushTool.fillRoundRect(x, y, size, size, size, size);
-            }
-        });
+    public void onBtnCircleClick(ActionEvent actionEvent) {
+        circleSelected ^= true;
     }
 
-    private void reDraw(GraphicsContext gcBrushTool) {
-        //gcBrushTool.clearRect(x1,y1,canvas.getWidth(),canvas.getHeight());
-        /*gcBrushTool.beginPath();
-        gcBrushTool.moveTo(x,y);
-        gcBrushTool.lineTo(x1,y1);
-        gcBrushTool.stroke();*/
-        line = new Line();
-        line.setStartX(x);
-        line.setStartY(y);
-        line.setEndX(x1);
-        line.setEndY(y1);
-        pane.getChildren().add(line);
-        //gcBrushTool.strokeLine(x,y,x1,y1);
+    @FXML
+    public void onBtnRectClick(ActionEvent actionEvent) {
+        rectangleSelected ^= true;
+    }
 
-        //flag=0;
+
+    public void onMouseDragged(MouseEvent mouseEvent) {
+        double x1=mouseEvent.getSceneX();
+        double y1=mouseEvent.getSceneY();
+
+        if (lineSelected) {
+            pane.getChildren().remove(line);
+            drawLine(x,y,x1,y1);
+        }else if(circleSelected) {
+            pane.getChildren().remove(circle);
+            drawCircle(x, y, x1, y1);
+        }else if(rectangleSelected) {
+            pane.getChildren().remove(rectangle);
+            drawRectangle(x, y, x1, y1);
+        }else if(brushSelected)
+            drawBrushStroke(x1, y1);
+        else if(checkBoxEraser.isSelected())
+            drawBrushStroke(x1, y1);
+    }
+
+    public void onMousePressed(MouseEvent mouseEvent) {
+        x = mouseEvent.getSceneX();
+        y = mouseEvent.getSceneY();
+    }
+
+    public void onMouseReleased(MouseEvent mouseEvent) {
+        double x1 = mouseEvent.getSceneX();
+        double y1 = mouseEvent.getSceneY();
+
+        if(lineSelected)
+            drawLine(x,y,x1,y1);
+        else if(circleSelected)
+            drawCircle(x, y, x1, y1);
+        else if(brushSelected)
+            drawBrushStroke(x1, y1);
+        else if(rectangleSelected)
+            drawRectangle(x, y, x1, y1);
+        else if(checkBoxEraser.isSelected())
+            drawBrushStroke(x1, y1);
+    }
+
+    public void drawLine(double startX, double startY, double endX, double endY){
+        this.line = new Line();
+
+        this.line.setStroke(colorPicker.getValue());
+        this.line.setStrokeWidth(sliderSize.getValue());
+        this.line.setStartX(startX);
+        this.line.setStartY(startY);
+        this.line.setEndX(endX);
+        this.line.setEndY(endY);
+        pane.getChildren().add(this.line);
+    }
+
+    public void drawCircle(double startX, double startY, double endX, double endY) {
+        this.circle = new Circle();
+
+        double diameter = Math.sqrt(Math.pow(endY-startY, 2) + Math.pow(endX-startX,2));
+        double centerY = (endY + startY) / 2;
+        double centerX = (endX + startX) / 2;
+
+        this.circle.setCenterX(centerX);
+        this.circle.setCenterY(centerY);
+        this.circle.setStroke(colorPicker.getValue());
+        this.circle.setStrokeWidth(sliderSize.getValue());
+        this.circle.setFill(Color.TRANSPARENT);
+        this.circle.setRadius(diameter/2);
+        pane.getChildren().add(circle);
+    }
+
+    public void drawBrushStroke(double currentX, double currentY)
+    {
+        this.brushStroke = new Circle();
+
+        if(brushSelected)
+            this.brushStroke.setFill(colorPicker.getValue());
+        else if(checkBoxEraser.isSelected())
+            this.brushStroke.setFill(Color.WHITE);
+
+        this.brushStroke.setRadius(sliderSize.getValue()/2);
+        this.brushStroke.setCenterX(currentX);
+        this.brushStroke.setCenterY(currentY);
+        pane.getChildren().add(brushStroke);
+    }
+
+    public void drawRectangle(double startX, double startY, double endX, double endY)
+    {
+        this.rectangle = new Rectangle();
+
+        if(endX > startX && endY > startY)
+        {
+            this.rectangle.setHeight((endY-startY));
+            this.rectangle.setWidth((endX-startX));
+            this.rectangle.setX(startX);
+            this.rectangle.setY(startY);
+        }
+        else if(endX < startX && endY > startY) {
+            this.rectangle.setHeight((endY-startY));
+            this.rectangle.setWidth((startX-endX));
+            this.rectangle.setX(endX);
+            this.rectangle.setY(startY);
+        }
+        else if(endX > startX && endY < startY) {
+            this.rectangle.setHeight(startY-endY);
+            this.rectangle.setWidth(endX-startX);
+            this.rectangle.setX(startX);
+            this.rectangle.setY(endY);
+        }
+        else if(endX < startX && endY < startY) {
+            this.rectangle.setHeight(startY-endY);
+            this.rectangle.setWidth(startX-endX);
+            this.rectangle.setX(endX);
+            this.rectangle.setY(endY);
+        }
+
+        this.rectangle.setStroke(colorPicker.getValue());
+        this.rectangle.setStrokeWidth(sliderSize.getValue());
+        this.rectangle.setFill(Color.TRANSPARENT);
+
+        pane.getChildren().add(rectangle);
     }
 
 }
