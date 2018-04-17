d9a907577552b437a6d7b0764bc9ade87819fb1e
alyaazab <alya.113@live.com>
2018-04-17 05:35:30 +0200

    Save-JSON (complete)
    line + rectangle

--- src/sample/files/JSONFile.java
+++ src/sample/files/JSONFile.java
@@ -2,9 +2,7 @@ package sample.files;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import sample.model.AbstractShape;
-import sample.model.ISaveLoadStrategy;
-import sample.model.Shape;
+import sample.model.*;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -14,27 +12,39 @@ public class JSONFile implements ISaveLoadStrategy {
     @Override
     public void save(ArrayList<Shape> shapes, String filepath) {
         JSONArray list = new JSONArray();
-        JSONObject jsonObject = new JSONObject();
 
-        for(int i=0; i<shapes.size(); i++)
-        {
-            jsonObject.put("name", ((AbstractShape)shapes.get(i)).getName());
-            list.add(jsonObject);
-            jsonObject.put("color", shapes.get(i).getColor().toString());
-            list.add(jsonObject);
-            jsonObject.put("strokeWidth", new Double(shapes.get(i).getStrokeWidth()));
-            list.add(jsonObject);
-            jsonObject.put("fillColor", shapes.get(i).getFillColor().toString());
-            list.add(jsonObject);
-            jsonObject.put("xPosition", new Double(shapes.get(i).getPosition().getX()));
-            list.add(jsonObject);
-            jsonObject.put("yPosition", new Double(shapes.get(i).getPosition().getY()));
-            list.add(jsonObject);
-        }
 
         try
         {
             FileWriter file = new FileWriter(filepath);
+
+            for(int i=0; i<shapes.size(); i++)
+            {
+                JSONObject jsonObject = new JSONObject();
+
+                jsonObject.put("name", ((AbstractShape)shapes.get(i)).getName());
+                jsonObject.put("color", shapes.get(i).getColor().toString());
+                jsonObject.put("strokeWidth", new Double(shapes.get(i).getStrokeWidth()));
+                jsonObject.put("fillColor", shapes.get(i).getFillColor().toString());
+
+                if(shapes.get(i) instanceof Line)
+                {
+                    jsonObject.put("start-x", ((Line)shapes.get(i)).getStartPoint().getX());
+                    jsonObject.put("start-y", ((Line)shapes.get(i)).getStartPoint().getY());
+                    jsonObject.put("emd-x", ((Line)shapes.get(i)).getEndPoint().getX());
+                    jsonObject.put("end-y", ((Line)shapes.get(i)).getEndPoint().getY());
+                }
+                else if(shapes.get(i) instanceof Rectangle)
+                {
+                    jsonObject.put("start-x", ((Rectangle)shapes.get(i)).getStartPoint().getX());
+                    jsonObject.put("start-y", ((Rectangle)shapes.get(i)).getStartPoint().getY());
+                    jsonObject.put("emd-x", ((Rectangle)shapes.get(i)).getEndPoint().getX());
+                    jsonObject.put("end-y", ((Rectangle)shapes.get(i)).getEndPoint().getY());
+                }
+
+                list.add(jsonObject);
+            }
+
             list.writeJSONString(file);
             file.flush();
             file.close();
